c74469b5586aa593e3b54eee0aada8cef5a66cd0
Derek <152582118+DerekJCR@users.noreply.github.com>
2025-06-24 15:03:13 -0600

    Con Angulare

--- backend/tienda_peluches/settings.py
+++ backend/tienda_peluches/settings.py
@@ -18,6 +18,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework', 
     'tienda',  
+    'corsheaders',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -28,9 +30,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'tienda_peluches.urls'
+CORS_ALLOW_ALL_ORIGINS = True
 
 TEMPLATES = [
     {
