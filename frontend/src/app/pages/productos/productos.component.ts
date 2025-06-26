2b3cae643d8e478904637794409a4f7edbb46f36
Derek <152582118+DerekJCR@users.noreply.github.com>
2025-06-26 13:23:52 -0600

    Avance Angular - Se añaden componentes

--- frontend/src/app/pages/productos/productos.component.ts
+++ frontend/src/app/pages/productos/productos.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ProductoService } from '../../services/producto.service';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-productos',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './productos.component.html',
-  styleUrl: './productos.component.scss'
+  styleUrls: ['./productos.component.scss']
 })
 export class ProductosComponent {
-
+  productos = [
+    { nombre: 'Peluche Pikachu', precio: 150 },
+    { nombre: 'Peluche Sans', precio: 200 },
+    { nombre: 'Peluche Freddy', precio: 180 }
+  ];
 }
