655a43f284b4ea6a5b4279bd5982005a9ac4a7e3
Carlos Adrián Hernández Torres <chernandezt1601@alumno.ipn.mx>
2025-06-29 10:37:58 -0600

    Version 1 (No estable)

---
+++ frontend/src/js/botones.js
@@ -0,0 +1,10 @@
+function inicio() {
+    var iframe = document.querySelector("iframe[name='info']");
+    iframe.src = 'inicio.html';
+}
+function index() {
+    window.location.href = 'index.html';
+}
+function registro() {
+    window.location.href = 'registro.html';
+}
